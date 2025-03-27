6eafa6d77cfe76c0aa035271a6a70a8f55e87baf
hp <Salari.hediye@gmail.com>
2025-03-27 19:20:26 +0430

    change initial migration

--- Data/Migrations/20250327143713_Initial.Designer.cs
+++ Data/Migrations/20250327143713_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250327140916_second")]
-    partial class second
+    [Migration("20250327143713_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
