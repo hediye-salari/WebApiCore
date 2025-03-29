5478616c65bbcc78ece4e9bcf6c91d7ed3a63db0
hp <Salari.hediye@gmail.com>
2025-03-29 20:55:58 +0430

    add repository in project and use repository for user and add method  for find user

---
+++ Data/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Common.Utilities;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+        //CancellationToken:در جایی که ارتباط قطع شد عملیات را کنسل میکنه 
+        // بر اساس یوزر و پسوورد کاربر را فایند میکند و بر میگرداند  
+        public Task<User> GetByUserAndPass(string username, string password, CancellationToken cancellationToken)
+        {
+            var passwordHash = SecurityHelper.GetSha256Hash(password);
+            return Table.Where(p => p.UserName == username && p.PasswordHash == passwordHash).SingleOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task AddAsync(User user, string password, CancellationToken cancellationToken)
+        {
+            var exists = await TableNoTracking.AnyAsync(p => p.UserName == user.UserName);
+            if (exists)
+                throw new Exception("نام کاربری تکراری است");
+
+            var passwordHash = SecurityHelper.GetSha256Hash(password);
+            user.PasswordHash = passwordHash;
+            await base.AddAsync(user, cancellationToken);
+        }
+    }
+}
---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Data.Repositories;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+
+    }
+}
